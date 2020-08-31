a6b37a1938544c5b24fc583ad85292e8cf5c7a9b
hexify <36350571+hexify1337@users.noreply.github.com>
2020-08-31 16:08:51 -0300

    Update threads.js

--- modules/threads.js
+++ modules/threads.js
@@ -1,24 +1,26 @@
-const { Worker } = require('worker_threads')
+const {
+    Worker
+} = require('worker_threads')
 const log = require('./logger')
 
 const LEADING_ZEROES = 4
 
-module.exports = 
-{
-    CreateThread: function (filename) 
-    {
-        const worker = new Worker(filename, { env: { LEADING_ZEROES } })
+module.exports = {
+    CreateThread: function (filename) {
+        const worker = new Worker(filename, {
+            env: {
+                LEADING_ZEROES
+            }
+        })
 
-        worker.on('error', function (error) 
-        {
+        worker.on('error', function (error) {
             log.error(error)
         })
 
-        worker.on('exit', function (code) 
-        {
+        worker.on('exit', function (code) {
             log.success(`Thread finished with code ${code}.`)
         })
-        
+
         log.info(`Thread initialized!`)
     }
 };
